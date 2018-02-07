f7b8e32fab4b1842b27ff0edefad7e8995ce0449
food869 <food869@naver.com>
2018-02-07 10:13:25 +0900

    2018-02-07 start

--- node-js-practice/selectdb.js
+++ node-js-practice/selectdb.js
@@ -12,7 +12,7 @@ client.query('SELECT * FROM user',(error,results)=>{
 	//console.log(JSON.stringify(results));
 	//results.forEach(function(item,index){
 	//	console.log(item);	
-	//});
+	//})
 	console.log(results.length);
 	for(var i = 0; i<results.length;i++){
 		console.log(results[i]);	
